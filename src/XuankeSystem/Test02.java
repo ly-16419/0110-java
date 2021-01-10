2f98f874493bbde9a0f0e2f881153d278d4eb48a
ly <1641911975@qq.com>
2021-01-10 21:06:15 +0800

    1.方法传递 2.类和对象 3.封装、继承

--- src/XuankeSystem/Test02.java
+++ src/XuankeSystem/Test02.java
@@ -4,14 +4,14 @@ public class Test02 {
         Student stu=new Student("计算机科学与技术","18076757556");
 
         stu.setId(123456789);
-        stu.setPassword("1111");
+         stu.setPassword("1111");
 
-        Student stu01=new Student("网络工程","18076757556");
+         Student stu01=new Student("网络工程","18076757556");
 
-        stu01.setId(987654321);
-        stu01.setPassword("2222");
+         stu01.setId(987654321);
+         stu01.setPassword("2222");
 
-        System.out.println(stu.getPhone());
-        System.out.println(stu.getMajor());
-    }
-}
+         System.out.println(stu.getPhone());
+         System.out.println(stu.getMajor());
+         }
+         }
