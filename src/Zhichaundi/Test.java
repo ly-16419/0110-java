1b88d22e541276b23646f44140c527add36568f2
ly <1641911975@qq.com>
2021-01-10 20:39:36 +0800

    方法传递

---
+++ src/Zhichaundi/Test.java
@@ -0,0 +1,15 @@
+package Zhichaundi;
+
+public class Test {
+    public static void main(String[] agre){
+        //值的传递，不改变a的值
+        int a=3;
+        m1(a);
+        System.out.println("aaa=" + a);
+    }
+    public static void m1(int a){
+        a=5;
+        System.out.println("ccc=" + a);
+    }
+
+}
